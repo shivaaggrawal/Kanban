9005fff8885c6ec33e8e26e2c4020d6237860b34
Shiva Agrawal <shiva5869agrawal@gmail.com>
2026-05-25 17:45:57 +0530

    implement task addition and deletion functionality; enhance task count update and improve nav styling

--- script.js
+++ script.js
@@ -1,8 +1,67 @@
+let tasksData = {}
+
+
 const todo = document.querySelector('#todo');
 const progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
 const columns = [todo, progress, done];
 let dragElement = null;
+
+function addTask(title, desc, column) {
+  const div = document.createElement("div")
+
+  div.classList.add("task")
+  div.setAttribute("draggable", "true")
+
+  div.innerHTML = `
+      <h2>${title}</h2>
+      <p>${desc}</p>
+      <button>Delete</button>`
+  column.appendChild(div)
+
+  div.addEventListener("drag", (e) => {
+    dragElement = div;
+  })
+
+  const deleteBtn = div.querySelector("button");
+  deleteBtn.addEventListener("click", () => {
+    div.remove();
+    updateTaskCount();
+  })
+
+  return div;
+}
+
+function updateTaskCount() {
+  columns.forEach(col => {
+    const tasks = col.querySelectorAll(".task");
+    const count = col.querySelector(".right");
+
+    tasksData[col.id] = Array.from(tasks).map(t => {
+      return {
+        title: t.querySelector("h2").innerText,
+        desc: t.querySelector("p").innerText
+      }
+
+    })
+    localStorage.setItem("tasks", JSON.stringify(tasksData));
+
+    count.innerText = tasks.length;
+  })
+}
+
+if (localStorage.getItem("tasks")) {
+  const data = JSON.parse(localStorage.getItem("tasks"));
+
+  for (const col in data) {
+    const column = document.querySelector(`#${col}`);
+    data[col].forEach(task => {
+      addTask(task.title, task.desc, column);
+    })
+  }
+
+  updateTaskCount();
+}
 
 const tasks = document.querySelectorAll('.task');
 
@@ -28,17 +87,10 @@ function addDragEventsOnColumn(column) {
   column.addEventListener("drop", (e) => {
     e.preventDefault();
 
-    console.log("dropped", dragElement, column);
-
     column.appendChild(dragElement);
     column.classList.remove("hover-over");
 
-    columns.forEach(col => {
-      const tasks = col.querySelectorAll(".task");
-      const count = col.querySelector(".right");
-
-      count.innerText = tasks.length;
-    })
+    updateTaskCount();
   })
 
 }
@@ -65,29 +117,10 @@ addTaskButton.addEventListener("click", () => {
   const taskTitle = document.querySelector("#task-title-input").value
   const taskDesc = document.querySelector("#task-desc-input").value
 
-  const div = document.createElement("div")
-
-  div.classList.add("task")
-  div.setAttribute("draggable", true)
-
-  div.innerHTML = `
-    <h2>${taskTitle}</h2>
-    <p>${taskDesc}</p>
-    <button>Delete</button>  
- `
-
-  todo.appendChild(div)
-
-  columns.forEach(col => {
-    const tasks = col.querySelectorAll(".task");
-    const count = col.querySelector(".right");
-
-    count.innerText = tasks.length;
-  })
-
-  div.addEventListener("drag", (e) => {
-    dragElement = div;
-  })
-
+  addTask(taskTitle, taskDesc, todo);
+  updateTaskCount();
   modal.classList.remove("active")
+
+  document.querySelector("#task-title-input").value = ""
+  document.querySelector("#task-desc-input").value = ""
 })
